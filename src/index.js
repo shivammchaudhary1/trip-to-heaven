09a5bc37a3cf3063d6ff9fdbf2d879065cbfff82
shivammchaudhary1 <shivamchaudhary75@gmail.com>
2023-02-21 11:47:20 +0530

    Add Dependencies

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </BrowserRouter>
+);
