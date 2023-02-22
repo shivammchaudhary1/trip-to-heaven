89a6861b10e70916ab4a47c4f4225d78a076f2c6
shivammchaudhary1 <shivamchaudhary75@gmail.com>
2023-02-22 21:07:55 +0530

    Hotel Page Navbar or Basic Structure

--- src/Pages/AllRoutes.jsx
+++ src/Pages/AllRoutes.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import {Routes, Route} from "react-router-dom"
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import { HomePage } from "./HomePage";
+import Hotel from "./Hotel/Hotel";
 
 export const AllRoutes = () => {
   return (
     <>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-        </Routes> 
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/hotel" element={<Hotel />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
