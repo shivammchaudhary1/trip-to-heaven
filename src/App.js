1538aff53b014b26163ccd91e5b386d937378521
https://github.com/arpit10saluja/git_we.git <arpitsaluja1234@gmail.com>
2023-02-22 23:11:48 +0530

    add flight page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+  </div>;
 }
 
 export default App;
---
+++ src/Pages/Flight.jsx
@@ -0,0 +1,5 @@
+export default function Flight(){
+    return <div>
+        flight
+    </div>
+}
