bb4332322be51bc924944ee7478c11af8cc4292d
shivammchaudhary1 <shivamchaudhary75@gmail.com>
2023-02-23 17:04:45 +0530

    Hotel File

--- src/Pages/Hotel/Hotel.jsx
+++ src/Pages/Hotel/Hotel.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Navbar } from "../../Components/Navbar";
+import HotelSubNavbar from "./HotelSubNavbar";
 
 const Hotel = () => {
   return (
     <div>
+      {/* use Navber Here Start */}
       <Navbar />
+      {/* use Navber Here End */}
+
+      <HotelSubNavbar />
     </div>
   );
 };
