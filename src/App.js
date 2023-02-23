a5c0e653fe73c62c97c1bce0eea495e29699376f
harshavardhan1612 <panchireddyharsha1612@gmail.com>
2023-02-23 07:30:20 +0530

    added bookingpage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import { AllRoutes } from "./Pages/AllRoutes";
+import { Booking } from "./Pages/booking/Booking";
 
 function App() {
   return (
       <>
         <AllRoutes />
+
+     
       </>
     )
 }
---
+++ src/Pages/booking/Booking.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Booking.Module.css'
+
+export const Booking = () => {
+  return (
+    <div className="booking">
+       
+        <h1>
+            Complete your Booking Process ...
+        </h1>
+
+    </div>
+  )
+}
