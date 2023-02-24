5a29acdc1b759bbf16711ecaeba38e52ea8acea0
sagarbalsaraf <sagarbalsaraf004@gmail.com>
2023-02-24 13:25:05 +0530

    add hotel admin UI

--- src/Pages/Admin/AdminHotel.jsx
+++ src/Pages/Admin/AdminHotel.jsx
@@ -1,34 +1,12 @@
-import React, { useState } from 'react'
 import { Box, Button, FormControl, FormLabel, Heading, Input } from '@chakra-ui/react'
-import { useDispatch } from 'react-redux'
 // import { addHotel } from '../../Redux/action'
 import styled from "styled-components"
 import "./Admin.Module.css"
 import { Link } from 'react-router-dom'
 
-let initialState = {
-        image: "",
-        place: "",
-        subtitle: "",
-        about: "",       
-}
+      
 export const AdminHotel = () => {
-    const [hotel, setHotel] = useState(initialState)  
-    const dispatch = useDispatch();
-    const handleChange = (e) => {
-        const  {name, value} = e.target;
-
-        setHotel((prev) => {
-            return {...prev, [name] : name==="price"? +value : value}
-             })     
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(hotel)
-      //  dispatch(addHotel(hotel));
-        setHotel(initialState)
-        
-    }
+   
   return (
     <div className='hotelBody'>
       <div id='link'>
@@ -39,23 +17,20 @@ export const AdminHotel = () => {
       
     <div>
         <Wrapper>
-        <form 
-        onSubmit={(e) => {
-            handleSubmit(e)
-        }}>
+        <form >
         <FormControl >
             <Heading id='head'>Admin Panel for Hotel</Heading>
             <Box className='hotelBox'>
             <FormLabel id='label'>Hotel Image</FormLabel>
-            <Input id='input' type="url" name="image" value={hotel.image} onChange={(e) => handleChange(e)}/>
+            <Input id='input' type="url" name="image" />
             <FormLabel  id='label'>Name</FormLabel>
-            <Input id='input' type="text" name='name' value={hotel.name} onChange={(e) => handleChange(e)}/>
+            <Input id='input' type="text" name='name' />
             <FormLabel  id='label'>Place</FormLabel>
-            <Input id='input' type="text" name='place' value={hotel.place} onChange={(e) => handleChange(e)}/>
+            <Input id='input' type="text" name='place' />
             <FormLabel  id='label'>Price</FormLabel>
-            <Input id='input' type="number" name='price' value={hotel.price} onChange={(e) => handleChange(e)}/>
+            <Input id='input' type="number" name='price'/>
             <FormLabel  id='label'>Description</FormLabel>
-            <Input id='input' type="text" name='description' value={hotel.description} onChange={(e) => handleChange(e)}/>
+            <Input id='input' type="text" name='description'/>
             <Button id='btn'  variant='outline' type='submit'>Add Hotel Info</Button>
             </Box>  
         </FormControl>
