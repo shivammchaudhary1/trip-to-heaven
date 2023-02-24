9c77c6b39252624b3279128f5b49eb9028567cec
https://github.com/arpit10saluja/git_we.git <arpitsaluja1234@gmail.com>
2023-02-24 23:24:22 +0530

    fw21_0696_day-4

--- src/Pages/Flights/FlightCard.jsx
+++ src/Pages/Flights/FlightCard.jsx
@@ -1,12 +1,24 @@
-import { Box, Image } from "@chakra-ui/react"
+import { Box, Image, Flex } from "@chakra-ui/react"
 
 export default function FlightCard({data}) {
     const {id, airline, from, to, departure, arrival, price, totalTime}= data
 
   return (
-    <Box display={"flex"} gap="10px" marginBottom={"20px"}>
+    <Box display={"flex"} gap="10px" marginBottom={"20px"} key={id}>
+        <Flex>
         <Image src="https://imgak.mmtcdn.com/flights/assets/media/dt/common/icons/I5.png?v=14" width={"25px"} />
         <h1>{airline}</h1>
+        </Flex>
+        <Flex>
+          {departure}
+          {from}
+        </Flex>
+        <Flex>
+          {arrival}
+          {to}
+        </Flex> 
+        <h2>{price}</h2>
+        <h3>`Duration${totalTime}`</h3>
     </Box>
     
   )
