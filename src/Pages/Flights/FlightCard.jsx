49fe3cd6065a8f9bff3bd1ed513f8ac4ce1630f6
https://github.com/arpit10saluja/git_we.git <arpitsaluja1234@gmail.com>
2023-02-26 11:53:08 +0530

    flight card

--- src/Pages/Flights/FlightCard.jsx
+++ src/Pages/Flights/FlightCard.jsx
@@ -8,21 +8,23 @@ export default function FlightCard({ data }) {
       gap="20px" key={id} height="100px"
       width={"80%"} boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
       padding="10px" margin="auto" justifyContent="space-around"
-      alignItems={"center"} borderRadius="10px" marginBottom={"20px"}>
-      <Box gap={"10px"}>
-        <Image src="https://imgak.mmtcdn.com/flights/assets/media/dt/common/icons/I5.png?v=14" width={"35px"} height="30px" />
+      alignItems={"center"} borderRadius="10px" marginBottom={"20px"} textAlign="center">
+      <Box gap={"30px"}>
+        <Image src="https://play-lh.googleusercontent.com/OhZSLjRDLvFLqtDp9bIgcvAweZIg5V5uIMI_7kOaS-9nPR043DUfoibkn1BgwG7Ai1U=w240-h480-rw" width={"35px"} height="30px" />
         <h1>{airline}</h1>
       </Box>
       <Flex display={"flex"} flexDirection="column">
+        <h3 style={{ fontSize: "10px", fontWeight: "bold" }}>Departure</h3>
         <h3>{departure}</h3>
         <b>{from} </b>
       </Flex>
       <Flex display={"flex"} flexDirection="column">
+        <h3 style={{ fontSize: "10px", fontWeight: "bold" }}>Arrival</h3>
         <h3>{arrival}</h3>
-        <b>{to} </b>
+        <b style={{ fontSize: "14px" }}>{to} </b>
       </Flex>
       <Flex display={"flex"} flexDirection="column">
-        <h3>Duation</h3>
+        <h3 >Duation</h3>
         <b>{totalTime}</b>
       </Flex>
       <Flex display={"flex"} flexDirection="column">
