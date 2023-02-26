bc09de90cc17522b8a112746f1ecd4e9f3ac8be4
sagarbalsaraf <sagarbalsaraf004@gmail.com>
2023-02-26 10:00:18 +0530

    hotelAdmin update

---
+++ src/Redux/AdminFlights/reducer.js
@@ -0,0 +1,27 @@
+import { FLIGHT_FAILURE, FLIGHT_REQUEST, GET_FLIGHT_SUCCESS, POST_FLIGHT_SUCCESS } from "./actionType"
+
+const initialState = {
+    data: [],
+    isLoading: false,
+    isError: false,
+}
+
+export const FlightReducer = (state= initialState, {type, payload}) =>{
+
+    switch (type){
+        case FLIGHT_REQUEST:
+        return {...state, isLoading: true}
+    
+        case FLIGHT_FAILURE:
+        return {...state, isError: true}
+    
+        case GET_FLIGHT_SUCCESS:
+        return {...state, isLoading: false, flight:payload}
+    
+        case POST_FLIGHT_SUCCESS:
+        return {...state, isLoading: false, flight: payload}
+    
+          default:
+            return state
+      }
+}
