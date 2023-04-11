4c5965a52866e093c9d52014a8881461db8f3aca
harshavardhan1612 <panchireddyharsha1612@gmail.com>
2023-04-11 16:38:01 +0530

    worked on responsiveness

--- src/Pages/booking/Cart.jsx
+++ src/Pages/booking/Cart.jsx
@@ -237,7 +237,7 @@ export const Cart = () => {
         <br />
 
 
-        <PaymentMsg />
+        <PaymentMsg  />
 
 
 
--- src/Pages/booking/Payment.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const Payment = () => {
-  return (
-    <div>payment</div>
-  )
-}
-
